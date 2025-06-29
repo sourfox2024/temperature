25b357491766117195a255ce5252d8c74aa724e7
Veronica <vronicasan@gmail.com>
2025-06-29 17:25:56 +1000

    refactor: move functions below main

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,35 @@
 use std::io::{self, Write};
 
-fn celsius_to_fahrenheit(celsius: f64) -> f64 {
-    celsius * 9.0 / 5.0 + 32.0
-}
-
 fn main() {
-    let mut celsius_input = String::new();
-    let celsius_value: f64;
-
-    loop {
-        print!("Enter the temperature in Celsius: ");
-        io::stdout().flush().expect("Failed to flush stdout"); // Ensure prompt is displayed immediately
-
-        celsius_input.clear(); // Clears the string for new input
-
-        io::stdin()
-            .read_line(&mut celsius_input)
-            .expect("Failed to read line");
-
-        match celsius_input.trim().parse::<f64>() {
-            Ok(number) => {
-                celsius_value = number;
-                break;
-            }
-            Err(_) => {
-                println!("That's not a valid number. Please try again.");
-            }
-        }
-    }
-
+    let celsius_value = get_temperature_from_user();
     let fahrenheit = celsius_to_fahrenheit(celsius_value);
 
     println!("
 Temperature in Celsius: {}°C", celsius_value);
     println!("Temperature in Fahrenheit: {}°F", fahrenheit);
+}
+
+fn celsius_to_fahrenheit(celsius: f64) -> f64 {
+    celsius * 9.0 / 5.0 + 32.0
+}
+
+fn get_temperature_from_user() -> f64 {
+    let mut input = String::new();
+    loop {
+        print!("Enter the temperature in Celsius: ");
+        io::stdout().flush().expect("Failed to flush stdout");
+
+        input.clear();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        match input.trim().parse::<f64>() {
+            Ok(number) => return number,
+            Err(_) => {
+                println!("That's not a valid number. Please try again.");
+            }
+        }
+    }
 }
